eadcc77bbc1bda99e67cdfa09fdf3d9577b20584
= <=>
2015-08-10 21:32:07 +0000

    Added basic temperature reading

--- temperature.py
+++ temperature.py
@@ -1,6 +1,18 @@
-#!/usr/bin/python3
+#!/opt/python3.4/bin/python3.4
+
+import re
 
 class temperature:
 
     def CurrentTemp(self, sensor):
-        return 32
+        file = open('/sys/bus/w1/devices/3b-00000018784c/w1_slave')
+        #foo = file.read()
+        #print(foo)
+        p = re.compile('t=[0-9]*')
+        m = p.search(file.read())
+        if m:
+            return m.group()
+        else:
+            return 'null'
+        #return re.match('t', foo)
+        #return 32
