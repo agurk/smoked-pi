0fd72e85e0ac787de5fc308a048669a0cae1e90d
agurk <timothy.moll+github@gmail.com>
2015-09-02 23:34:50 +0300

    Improved error handling and prettified formatting

--- temperature.py
+++ temperature.py
@@ -5,14 +5,20 @@ import re
 class temperature:
 
     def CurrentTemp(self, sensor):
-        file = open('/sys/bus/w1/devices/3b-00000018784c/w1_slave')
-        #foo = file.read()
-        #print(foo)
-        p = re.compile('t=[0-9]*')
-        m = p.search(file.read())
-        if m:
-            return m.group()
-        else:
-            return 'null'
-        #return re.match('t', foo)
-        #return 32
+        rawC = int(self.GetRawTemp(sensor))
+        rawF = (rawC * 9) / 5 + 32
+        prettyC = str("{0:.2f}".format(rawC/1000)) + ' C'
+        prettyF = str("{0:.1f}".format(rawF/1000)) + ' F'
+        return prettyC + '  |  ' + prettyF
+
+    def GetRawTemp(self, sensor):
+        try:
+            file = open('/sys/bus/w1/devices/'+sensor+'/w1_slave', 'r')
+            p = re.compile('t=([0-9]*)')
+            m = p.search(file.read())
+            if m:
+                return m.groups(1)[0]
+        except FileNotFoundError:
+            return '-373260'
+        return '-373260'
+
