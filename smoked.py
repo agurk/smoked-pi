0aac814d7e7522abef55da22660d21256bf0d085
agurk <timothy.moll+github@gmail.com>
2015-09-02 23:35:10 +0300

    Changed sensor handling to be from defined dictionary

--- smoked.py
+++ smoked.py
@@ -7,18 +7,30 @@ app = Flask(__name__)
 from temperature import temperature
 import json
 
+temp = temperature()
+
+sensors={}
+sensors['inlet_temp'] = 'aoeu'
+sensors['exhaust_temp'] = 'aoeu'
+sensors['chamber_temp_1'] = 'aoeu'
+sensors['chamber_temp_2'] = 'aoeu'
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/temp/<sensor>')
 def getTemperature(sensor):
-    temp=temperature()
     foo={}
-    foo['inlet_temp']=temp.CurrentTemp(sensor)
-    foo['exhaust_temp']=temp.CurrentTemp(sensor)
-    foo['chamber_temp_1']=temp.CurrentTemp(sensor)
-    foo['chamber_temp_2']=temp.CurrentTemp(sensor)
+    if sensor in sensors.keys():
+        foo[sensor]=temp.CurrentTemp(sensors[sensor])
+    return str(json.JSONEncoder().encode(foo))
+
+@app.route('/temps/')
+def getTemperatures():
+    foo={}
+    for sensor in sensors:
+        foo[sensor]=temp.CurrentTemp(sensors[sensor])
     return str(json.JSONEncoder().encode(foo))
 
 if __name__ == '__main__':
