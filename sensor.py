4fac583d6611401fa71c721fdccea0dcd18b7fbe
agurk <timothy.moll+github@gmail.com>
2015-09-08 23:00:19 +0300

    Pushed sensors into their new own module, separating sensing logic from reading the temperature of the python object

---
+++ sensor.py
@@ -0,0 +1,67 @@
+#!/opt/python3.4/bin/python3.4
+
+import re
+from statistics import mean
+import threading
+import time
+
+class sensor:
+
+    sampleSize = 5
+    samplePosition = 0
+    startingTemp = 25
+    sleepTime = 0.2
+
+    def __init__(self, name):
+        self.name=name
+        self.temps = [self.startingTemp] * self.sampleSize
+
+    def Temperature(self):
+        return mean(self.temps)
+        
+    def UpdateTemp(self):
+        self.samplePosition += 1
+        self.samplePosition = self.samplePosition % self.sampleSize
+        self.temps[self.samplePosition] = self.RawTemp()
+
+    def Run(self):
+        while 1:
+            self.UpdateTemp()
+            time.sleep(self.sleepTime)
+
+    def Start(self):
+        t = threading.Thread(target=self.Run)
+        t.daemon = True
+        t.start()
+
+class thermocouple(sensor):
+
+    def __init__(self, name, w1id, sumOffset=0, productOffset=1):
+        super().__init__(name)
+        self.w1id = w1id
+        self.sumOffset = sumOffset
+        self.productOffset = productOffset
+
+    def RawTemp(self):
+        raw = self.RawTempString()
+        if raw == '':
+            raw = '0'
+        return (int(raw) / 1000) * self.productOffset + self.sumOffset
+
+    def RawTempString(self):
+        try:
+            file = open('/sys/bus/w1/devices/'+self.w1id+'/w1_slave', 'r')
+            p = re.compile('t=([0-9]*)')
+            m = p.search(file.read())
+            if m:
+                return m.groups(1)[0]
+        except FileNotFoundError:
+            return '-373260'
+        return '-373260'
+
+class thermistor(sensor):
+
+    def __init__(self, name, w1id, sumOffset=0, productOffset=1):
+        self.name = name
+        self.w1id = w1id
+        self.sumOffset = sumOffset
--- temperature.py
+++ temperature.py
@@ -2,54 +2,31 @@
 
 import re
 
+from sensor import thermocouple
+from sensor import thermistor
+
 class temperature:
 
     sensors={}
 
     def __init__(self):
-        self.sensors['inlet_temp'] = sensor('inlet_temp', '3b-00000018784c')
-        self.sensors['exhaust_temp'] = sensor('exhaust_temp', '3b-0000001916d9')
-        self.sensors['chamber_temp_1'] = sensor('chamber_temp_1', '3b-000000191fc3')
-        self.sensors['chamber_temp_2'] = sensor('chamber_temp_2', 'aoeu')
-        self.sensors['food_temp_1'] = sensor('food_temp_1', 'aoeu')
-        self.sensors['food_temp_2'] = sensor('food_temp_2', 'aoeu')
+        self.sensors['inlet_temp'] = thermocouple('inlet_temp', '3b-000000191eb6')
+        self.sensors['inlet_temp'].Start()
+        self.sensors['exhaust_temp'] = thermocouple('exhaust_temp', '3b-000000191db5')
+        self.sensors['exhaust_temp'].Start()
+        self.sensors['chamber_temp_1'] = thermocouple('chamber_temp_1', '3b-000000191713')
+        self.sensors['chamber_temp_1'].Start()
+        self.sensors['chamber_temp_2'] = thermocouple('chamber_temp_2', '3b-0000001916d9')
+        self.sensors['chamber_temp_2'].Start()
+        self.sensors['food_temp_1'] = thermocouple('food_temp_1', 'aoeu')
+        self.sensors['food_temp_2'] = thermocouple('food_temp_2', 'aoeu')
 
     def CurrentTemp(self, sensorName):
         sensor = self.sensors[sensorName]
         rawC = sensor.Temperature()
         rawF = (rawC * 9) / 5 + 32
-        prettyC = str("{0:.2f}".format(rawC)) + ' C'
-        prettyF = str("{0:.1f}".format(rawF)) + ' F'
+        prettyC = str("{0:.0f}".format(rawC)) + ' C'
+        prettyF = str("{0:.0f}".format(rawF)) + ' F'
         return prettyC + '  |  ' + prettyF
 
-
-class sensor:
-
-    name=''
-    w1id=''
-    sumOffset=0
-    productOffset=1
-
-    def __init__(self, name, w1id, sumOffset=0, productOffset=1):
-        self.name = name
-        self.w1id = w1id
-        self.sumOffset = sumOffset
-        self.productOffset = productOffset
-
-    def Temperature(self):
-        raw = self.RawTemp()
-        if raw == '':
-            raw = '0'
-        return (int(raw) / 1000) * self.productOffset + self.sumOffset
-
-    def RawTemp(self):
-        try:
-            file = open('/sys/bus/w1/devices/'+self.w1id+'/w1_slave', 'r')
-            p = re.compile('t=([0-9]*)')
-            m = p.search(file.read())
-            if m:
-                return m.groups(1)[0]
-        except FileNotFoundError:
-            return '-373260'
-        return '-373260'
 
