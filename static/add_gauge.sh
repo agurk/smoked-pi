dce9bed5aceb9eed332bc02574bcdaee86c5ae8a
agurk <timothy.moll+github@gmail.com>
2015-11-26 23:16:36 +0200

    Improve the gauge svg and use it for all temp readings

---
+++ static/add_gauge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i "s/id=\"gauge\"/id=\"gauge_$2\"/" $1
+sed -i "s/id=\"needle\"/id=\"gauge_$2-needle\"/" $1
+sed -i "s/id=\"temp_c\"/id=\"gauge_$2-temp_c\"/" $1
+sed -i "s/id=\"temp_f\"/id=\"gauge_$2-temp_f\"/" $1
+sed -i "s/id=\"no_connection\"/id=\"gauge_$2-no_connection\"/" $1
--- temperature.py
+++ temperature.py
@@ -10,12 +10,12 @@ class temperature:
         self.adc = ADS1x15(ic=0x01)
         self.adc.Start()
         self.sensors={}
-        self.sensors['inlet_temp'] = thermocouple('inlet_temp', '3b-000000191eb6')
-        self.sensors['exhaust_temp'] = thermocouple('exhaust_temp', '3b-000000191db5')
-        self.sensors['chamber_temp_1'] = thermocouple('chamber_temp_1', '3b-000000191713')
-        self.sensors['chamber_temp_2'] = thermocouple('chamber_temp_2', '3b-000000191fc3')
-        self.sensors['food_temp_1'] = thermistor('food_temp_1', 0, self.adc)
-        self.sensors['food_temp_2'] = thermistor('food_temp_2', 1, self.adc)
+        self.sensors['gauge_1'] = thermistor('food_temp_1', 0, self.adc)
+        self.sensors['gauge_2'] = thermistor('food_temp_2', 1, self.adc)
+        self.sensors['gauge_3'] = thermocouple('inlet_temp', '3b-000000191eb6')
+        self.sensors['gauge_4'] = thermocouple('exhaust_temp', '3b-000000191db5')
+        self.sensors['gauge_5'] = thermocouple('chamber_temp_1', '3b-000000191713')
+        self.sensors['gauge_6'] = thermocouple('chamber_temp_2', '3b-000000191fc3')
         for key, sensor in self.sensors.items():
             sensor.Start()
 
@@ -23,9 +23,10 @@ class temperature:
         sensor = self.sensors[sensorName]
         rawC = sensor.Temperature()
         rawF = (rawC * 9) / 5 + 32
-        prettyC = str("{0:.1f}".format(round(rawC, 1))) + ' C'
-        prettyF = str("{0:.0f}".format(round(rawF, 0))) + ' F'
-        return prettyC + '  |  ' + prettyF
+        temp={}
+        temp['c'] = str("{0:.1f}".format(round(rawC, 1)))# + ' C'
+        temp['f'] = str("{0:.0f}".format(round(rawF, 0)))# + ' F'
+        return temp
 
     def Change(self, sensorName):
         sensor = self.sensors[sensorName]
